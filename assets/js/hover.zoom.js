27f44a608c1dc9ee4aff0ece25c5ec529dc02b2c
Joyce Yan <joyceyan@usc.edu>
2015-02-01 00:04:47 -0800

    blue -> gray

--- assets/js/hover.zoom.js
+++ assets/js/hover.zoom.js
@@ -6,7 +6,7 @@
 
             var defaults = {
                 overlay: true,
-                overlayColor: '#2e9dbd',
+                overlayColor: '#34495e',
                 overlayOpacity: 0.9,
                 zoom: 25,
                 speed: 300
